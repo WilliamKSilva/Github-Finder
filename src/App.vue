66f3028ec7a8831499c21a40c9ed34b37997dd82
Williamkds <wilz.kelvin12@gmail.com>
2021-11-28 18:16:17 -0300

    Making the page a little more responsive.

--- src/App.vue
+++ src/App.vue
@@ -30,14 +30,42 @@ export default {
   text-align: center;
   color: #ffffff;
 }
-body {
-  background-color: #00171f;
-}
 
-* {
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  html {
+    @media (max-widhth: 1080px) {
+      font-size: 93.75%;
+    }
+    @media (max-widhth: 720px) {
+      font-size: 87.5%;
+    }
+  }
+  body {
+     background: #00171f;
+    -webkit-font-smoothing: antialised;
+  }
+  body, input, textarea, button {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+  }
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 600;
+  }
+  button {
+    cursor: pointer;
+  }
+  [disabled] {
+    opacity: 0.6;
+    cursor: not-allowed;
+  }
+
+main {
+  display: flex;
+  margin-top: 8rem;
 }
 
 main {
